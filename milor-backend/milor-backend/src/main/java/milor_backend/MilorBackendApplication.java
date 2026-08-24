80d2e89b31187cbc879ec72a4def8bf06761b604
Oscar Guerrero <oscarmanuelgf@gmail.com>
2026-08-24 04:28:24 -0500

    Forzar zona horaria America/Lima en la aplicacion

--- milor-backend/milor-backend/src/main/java/milor_backend/MilorBackendApplication.java
+++ milor-backend/milor-backend/src/main/java/milor_backend/MilorBackendApplication.java
@@ -3,11 +3,19 @@ package milor_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
-public class 	MilorBackendApplication {
+public class MilorBackendApplication {
+
+	// Forzar la zona horaria de Perú (America/Lima) para toda la aplicación
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MilorBackendApplication.class, args);
 	}
-
 }
